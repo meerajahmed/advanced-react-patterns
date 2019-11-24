341a795b2594a9076e1d57b43b16a3aa74f29d30
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2019-11-24 23:52:01 +0530

    Add bootstrap styled

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const App = () => <h1>Hello World!</h1>;
+import BootstrapProvider from '@bootstrap-styled/provider';
+import { Header, H1 } from '@bootstrap-styled/v4';
+
+
+const App = () => (
+    <BootstrapProvider reset={true}>
+        <Header className="d-flex justify-content-center my-5 p-2">
+            <H1 color="muted">Hello World!</H1>
+        </Header>
+    </BootstrapProvider>
+    );
 render(<App />, document.getElementById('root'));
