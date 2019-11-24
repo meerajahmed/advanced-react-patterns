5df574f58300b567764c8e2a7a972c0da634c293
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2019-11-24 20:05:23 +0530

    CoInitial app config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash:8].js'
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            favicon: './src/assets/icons/favicon.png'
+        })
+    ]
+};
+
+module.exports = config;
